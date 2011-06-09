b8757c4be51cddac258923cb3decb8d1fe20dce8
Laurent <lbuffevant@gmail.com>
2011-06-09 09:58:08 +0200

    init limouzi estimate project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Limouzi::Application.config.secret_token = 'b975e8bd21334e17fcd7dcabe011d0a42210f45c46612b2ff0ce8fd31fe499307929c6e73966254edbc01cbb8c5d752663ec176a08b201339aea23189b1edd20'
